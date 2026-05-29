6756756184dd8f3b3795c8cf8f5a0492a2e7052a
Ever-VC <ever.vasquezc27@gmail.com>
2026-05-29 11:40:57 -0600

    🚧 Work in Progress:  Validaciones del login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SignInRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function index() {
+    public function index() 
+    {
         return view('auth.login');
+    }
+
+    public function store(SignInRequest $request) 
+    {
+        $data = $request->validated();
+
+        if (!Auth::attempt($data)) {
+            return back()->with('error', 'Credenciales incorrectas');
+        }
     }
 }
---
+++ app/Http/Requests/SignInRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Override;
+
+class SignInRequest extends FormRequest
+{
+
+    #[Override]
+    public function attributes()
+    {
+        return [
+            'password' => 'contraseña'
+        ];
+    }
+
+    #[Override]
+    public function messages(): array
+    {
+        return [
+            'email.exists' => 'No se ha encontrado una cuenta con ese correo.'
+        ];
+    }
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email', 'exists:users,email'],
+            'password' => ['required']
+        ];
+    }
+}
