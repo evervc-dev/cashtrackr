4d3451fce0323044d0d512316787a27a947374ed
Ever-VC <ever.vasquezc27@gmail.com>
2026-05-29 11:29:56 -0600

    💄 UI:  Mensaje de exito con texto color verde

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,6 @@
 
 @section('auth-contents')
     @if (session('success'))
-        <p class="my-10 text-center border border-green-400 bg-green-100 py-3 text-sm">{{ session('success') }}</p>
+        <p class="my-10 text-center border border-green-400 bg-green-100 text-green-700 py-3 text-sm">{{ session('success') }}</p>
     @endif
 @endsection
