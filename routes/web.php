f5681cb723ade3acc8ee638265c1185e3f4c8c2c
Ever-VC <ever.vasquezc27@gmail.com>
2026-05-29 20:38:01 -0600

    ✨ New Feature: Reenvío de mensaje de confirmación al correo

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -26,6 +28,15 @@ Route::get('/email/verify/{id}/{hash}', function(EmailVerificationRequest $reque
 Route::get('/email/verify', function() {
     return view('auth.verify-email');
 })->middleware('auth')->name('verification.notice');
+
+Route::post('/email/verification-notification', function(Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('success', 'Se ha reenviado el correo de verificación');
+
+    //Rate limit con throttle donde el primer 1 es la cantidad de peticiones que se pueden enviar
+    // y el otro 1 es en la cantidad de tiempo que se pueden enviar, es decir, lo siguiente permite 1 peticón en cada (1) minuto
+})->middleware(['auth', 'throttle:1,1'])->name('verification.send');
 
 Route::get('dashboard', function() {
     return view('dashboard');
