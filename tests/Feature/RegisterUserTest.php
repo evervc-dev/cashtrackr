99c2a6af4e6b738dde0939baa935b8d9127035bf
Ever-VC <ever.vasquezc27@gmail.com>
2026-05-31 22:12:02 -0600

    ✅ Adding Tests: Happy Path de registro de un usuario

--- tests/Feature/RegisterUserTest.php
+++ tests/Feature/RegisterUserTest.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+
+uses(RefreshDatabase::class);
+
 it('shows the registration screen', function() {
     $response = $this->get(route('register'));
 
@@ -15,4 +22,28 @@ it('shows the registration screen', function() {
         'Crear Cuenta',
         'Registrarme'
     ]);
+});
+
+
+it('Register a new user unverified and dispatches the registered event', function() {
+    // Evita que se envíen los emails (para evitar el consumo)
+    Event::fake();
+
+    $response = $this->post(route('register.store'), [
+        'name' => 'Juan Pérez',
+        'email' => 'juanperez23@gamil.com',
+        'password' => 'Xq7#vL2!pR9@tF3%',
+        'password_confirmation' => 'Xq7#vL2!pR9@tF3%'
+    ]);
+
+    $response->assertRedirect(route('verification.notice'));
+
+    $user = User::whereEmail('juanperez23@gamil.com')->first();
+
+    expect($user)->not()->toBeNull();
+    expect($user->name)->toBe('Juan Pérez');
+    expect($user->email)->toBe('juanperez23@gamil.com');
+    expect($user->hasVerifiedEmail())->toBeFalse(); // Indica que la cuenta aún no esté verificada
+
+    Event::assertDispatched(Registered::class);
 });
