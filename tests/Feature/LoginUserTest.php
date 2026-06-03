ef6ae99654b5e873a70679ffe9c042591b086cf3
Ever VC <ever.vasquezc27@gmail.com>
2026-06-02 20:40:21 -0600

    ✅ Adding Tests: Inicio de sesión de un usuario

--- tests/Feature/LoginUserTest.php
+++ tests/Feature/LoginUserTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
@@ -8,4 +9,20 @@ it('Shows the login screen', function () {
     $response = $this->get('/login');
 
     $response->assertOk();
+});
+
+it('Logs in a verified user successfully', function () {
+    $user = User::factory()->create([
+        'email' => 'juanperez23@gamil.com',
+        'password' => bcrypt('Xq7#vL2!pR9@tF3%'),
+        'email_verified_at' => now(),
+    ]);
+
+    $response = $this->post(route('login.store'), [
+        'email' => $user->email,
+        'password' => 'Xq7#vL2!pR9@tF3%',
+    ]);
+
+    $response->assertRedirect(route('dashboard'));
+    $this->assertAuthenticated();
 });
