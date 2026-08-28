5bb6eaac9e71989e8d3c6dc6c8cec3423f84ad79
Ever-VC <ever.vasquezc27@gmail.com>
2026-08-28 09:55:28 -0600

    💄 UI:  Modificación del color de texto en la opción de eliminar

--- resources/views/components/budget-dropdown.blade.php
+++ resources/views/components/budget-dropdown.blade.php
@@ -22,7 +22,7 @@
                 type="button"
                 command="show-modal"
                 commandfor="delete-dialog-"
-                class="block w-full px-4 py-2 text-left text-sm text-gray-700 focus:bg-gray-100 focus:text-gray-900"
+                class="block w-full px-4 py-2 text-left text-sm text-gray-700 focus:bg-gray-100 focus:text-gray-900 focus:outline-hidden dark:text-gray-300 dark:focus:bg-white/5 dark:focus:text-white"
             >
                 Eliminar
             </button>
